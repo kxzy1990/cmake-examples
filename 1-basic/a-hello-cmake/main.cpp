1ea4fc6071966dbac819224859433fd69e8dd24d
ivan <kxzy1990@163.com>
2024-11-03 17:50:49 +0800

    test

--- 1-basic/a-hello-cmake/main.cpp
+++ 1-basic/a-hello-cmake/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello CMake!" << std::endl;
+   unsigned char a = 0;
+   string name ="lkz";
+      cout << name.size()  << endl;
+
+   for (uint8_t i = 0; i < 0xff; i++)
+   {
+      // stringstream ss;
+      // ss <<  "|" << to_string(i) << "|";
+   }
    return 0;
 }
